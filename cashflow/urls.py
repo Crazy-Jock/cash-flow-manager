f528b12dc3c247eedf37af7d9066ed36c1f8e284
Crazy_Jock <jockmail001@gmail.com>
2026-06-29 22:10:51 +0300

    feat: main page with table

---
+++ cashflow/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from cashflow import views
+
+urlpatterns = [
+    path("", views.cashflow_list, name="list"),
+]
--- cashflow/views.py
+++ cashflow/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from cashflow.models import Type, Status, Category, SubCategory, CashFlow
 
+
+def cashflow_list(request):
+    items = CashFlow.objects.all().select_related("status", "type", 
+                                                  "category", "subcategory")
+    
+    return render(request, "cashflow/list.html", {"items": items})
